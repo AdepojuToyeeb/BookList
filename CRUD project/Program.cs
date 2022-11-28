914cab61aa7e7dd6f168a8fda44e7cec6edb63ca
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-11-28 09:08:40 +0100

    Added Runtime package Package

--- CRUD project/Program.cs
+++ CRUD project/Program.cs
@@ -1,7 +1,11 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
